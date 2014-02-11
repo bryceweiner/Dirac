bcb4b3e670463e54b2037599a50003ae39f22648
Bryce Weiner <bryce.weiner23@gmail.com>
2014-02-10 23:17:30 -0800

    mroe dumb shit

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,6 @@
 #include "init.h"
 #include "ui_interface.h"
 #include "checkqueue.h"
-#include "clone.h"
 #include <boost/algorithm/string/replace.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/filesystem/fstream.hpp>
