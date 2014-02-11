ca63391f4076c63b63e32bf9813bd5edace734c5
Bryce Weiner <bryce.weiner23@gmail.com>
2014-02-10 23:58:35 -0800

    genesis.h stuff

--- src/genesis.h
+++ src/genesis.h
@@ -2,7 +2,7 @@ uint256 hashGenesisBlockMainNet("0x");
 uint256 hashMerkleRootMainNet("0x");
 uint256 hashGenesisBlockTestNet("0x");
 uint256 hashMerkleRootTestNet("0x");
-static const unsigned int timeTestNetBlock = 1381036817;
+static const unsigned int timeGenesisBlockTestNet = 1381036818;
 static const unsigned int timeGenesisBlockMainNet = 1381036817;
 const char* pszTimestamp = "Not with a bang, but a whimper.";
 static const unsigned int nTestNonce = 120396719;
